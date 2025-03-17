c58da17d3326f5432341ab8871ecf199b5481b06
BananKing1 <159125469+BananKing1@users.noreply.github.com>
2025-03-17 14:35:59 +0100

    csd

--- Index.php
+++ Index.php
@@ -9,10 +9,13 @@
 
 <body>
     <header>
-        <img src="Bilder/Icons/task.png" alt="task">
+        <div class="group">
+            <img src="Bilder/logo.png" alt="logo">
+        </div>
+        <img src="Bilder/Icons/task.png" alt="task" onclick="window.location.href = 'task.php';">
         <img src="Bilder/Icons/mail.png" alt="mail">
         <img src="Bilder/Icons/bar-chart.png" alt="bar-chart">
-        <div class="user">
+        <div class="group">
             <img src="Bilder/Icons/notification.png" alt="notification">
             <img src="Bilder/Icons/user.png" alt="user">
         </div>
