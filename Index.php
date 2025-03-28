5f1dc45ec45b2aff98ac01b115bffbc165170fb4
BananKing1 <159125469+BananKing1@users.noreply.github.com>
2025-03-28 11:12:19 +0100

    dasf

--- Index.php
+++ Index.php
@@ -5,20 +5,43 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="stylesheet.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Tienne:wght@400;700;900&display=swap" rel="stylesheet">
 </head>
 
 <body>
     <header>
-        <div class="group">
-            <img src="Bilder/logo.png" alt="logo">
-        </div>
-        <img src="Bilder/Icons/task.png" alt="task" onclick="window.location.href = 'task.php';">
-        <img src="Bilder/Icons/mail.png" alt="mail">
-        <img src="Bilder/Icons/bar-chart.png" alt="bar-chart">
-        <div class="group">
+    <div class="vertical_align"></div>
+            <img src="Bilder/Logo.webp" alt="logo">
+        
+            <div class="filler"></div>
+        
+            <img src="Bilder/Icons/home.png" alt="home">
+            <img src="Bilder/Icons/task.png" alt="task" onclick="window.location.href = 'task.php';">
+            <img src="Bilder/Icons/mail.png" alt="mail">
+            <img src="Bilder/Icons/bar-chart.png" alt="bar-chart">
+
+            <div class="filler"></div>
+
             <img src="Bilder/Icons/notification.png" alt="notification">
             <img src="Bilder/Icons/user.png" alt="user">
         </div>
     </header>
+
+    <div class="rest">
+        <div class="StatusUpdate">
+            Status uppatering
+            <div>
+                Nytt:
+            </div>
+            <div>
+                Pågående:
+            </div>
+            <div>
+                Slutförd:
+            </div>
+        </div>
+    </div>
 </body>
 </html>
