af5d2fd0a5fd4432afa072c5c69c4c3ff804bb66
BananKing1 <159125469+BananKing1@users.noreply.github.com>
2025-03-31 12:05:51 +0200

    dfghj

--- Index.php
+++ Index.php
@@ -17,23 +17,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="stylesheet.css">
+    <link rel="stylesheet" href="indexstyle.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Tienne:wght@400;700;900&display=swap" rel="stylesheet">
 </head>
 
 <body>
+    <div class="header"></div>
+
     <header>
     <div class="vertical_align"></div>
             <img src="Bilder/Logo.webp" alt="logo">
         
             <div class="filler"></div>
         
-            <img src="Bilder/Icons/home.png" alt="home">
-            <img src="Bilder/Icons/task.png" alt="task" onclick="window.location.href = 'task.php';">
-            <img src="Bilder/Icons/mail.png" alt="mail">
-            <img src="Bilder/Icons/bar-chart.png" alt="bar-chart">
+            <img src="Bilder/Icons/mail.png" alt="mail" onclick="location.href='Mail.php'">
+            <img src="Bilder/Icons/home.png" alt="home" onclick="location.href='Index.php'">
+            <img src="Bilder/Icons/bar-chart.png" alt="bar-chart" onclick="location.href='Charts.php'">
 
             <div class="filler"></div>
 
@@ -43,19 +44,34 @@
     </header>
 
     <div class="rest">
-<?php
-    $sql = "SELECT * FROM tblmatters";
-    $result = mysqli_query($conn, $sql);
-    while($row=mysqli_fetch_assoc($result)){ 
+        Kritisk prioritering:
+        <div class="box">
+            <?php
+        $sql = "SELECT * FROM tblmatters WHERE (status='open' OR status='ongoing') AND priority='critical' ORDER BY created DESC";
+        $result = mysqli_query($conn, $sql);
+        while($row=mysqli_fetch_assoc($result)){ 
+            ?>
+            <div class="StatusUpdate">
+                <h2>Ärende: <?=$row['matters']?></h2>
+                <h2>Av: <?=$row['rapport']?></h2>
+                <h2>Kontakt: <?=$row['contact']?></h2>
+                <h2>Skapad: <?=$row['created']?></h2>
+                <h2>Senast uppdaerad: <?=$row['updated']?></h2>
+                <h2>Beskrivning: <?=$row['beskrivning']?></h2>
+                <h2>Status: <?=$row['status']?></h2>
+
+                <button onclick="Finish()">Slutföra</button>
+            </div>
+        <?php }
         ?>
-        <div class="StatusUpdate">
-            <h2>Ärende: <?=$row['matters']?></h2>
-            <h2>Beskrivning: <?=$row['beskrivning']?></h2>
-            <h2>Status: <?=$row['status']?></h2>
-            <h2>Prioritet: <?=$row['priority']?></h2>
         </div>
-    <?php }
-?>
+
+        <br>Hög prioritering:
+
+        <br>Medium prioritering:
+
+        <br>Låg prioritering:
+
     </div>
 </body>
 </html>
